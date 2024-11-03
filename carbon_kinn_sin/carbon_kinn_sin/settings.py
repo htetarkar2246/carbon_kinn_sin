07af17847d369789b0ea4c3d79b32ede70b41aa6
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-11-03 17:59:18 +0630

    Update settings.py

--- carbon_kinn_sin/carbon_kinn_sin/settings.py
+++ carbon_kinn_sin/carbon_kinn_sin/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,14 +84,7 @@ WSGI_APPLICATION = 'carbon_kinn_sin.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carbon_kinn_sin',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',  
-        'PORT': '5432',      
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
